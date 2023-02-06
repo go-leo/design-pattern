a51395ff44841c33f39213298d988f3d8fe84d62
soyacen <soyacen@outlook.com>
2023-02-06 14:52:25 +0800

    add DomainEvent

--- ddd/domain_event.go
+++ ddd/domain_event.go
@@ -1,13 +1,35 @@
 package ddd
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 // DomainEvent is something that is unique, but does not have a lifecycle.
 // The identity may be explicit, for example the sequence number of a payment,
 // or it could be derived from various aspects of the event such as where, when and what
 // has happened.
-type DomainEvent[T any] interface {
+type DomainEvent[T any, ID any] interface {
 	SameEventAs(other T) bool
+	ID() ID
+	Kind() string
+	When() time.Time
+}
+
+type EventDescriptor struct {
+	ID         int64
+	Body       string
+	OccurredAt time.Time
+	Kind       string
+}
+
+func NewEventDescriptor(body string, occurredAt time.Time, kind string) *EventDescriptor {
+	return &EventDescriptor{Body: body, OccurredAt: occurredAt, Kind: kind}
+}
+
+func EventDescriptorFromEvent[T any, ID any](e DomainEvent[T, ID]) *EventDescriptor {
+	data, _ := json.Marshal(e)
+	return NewEventDescriptor(string(data), e.When(), e.Kind())
 }
 
 type domainEvent[T any] struct {
@@ -15,3 +37,9 @@ type domainEvent[T any] struct {
 	Version   string
 	Timestamp time.Time
 }
+
+///Users/songyancheng/Workspace/DDD/clean-arch-ddd-intro/src/main/java/com/github/felpexw/shared/domain/common
+///Users/songyancheng/Workspace/DDD/event-source-cqrs-sample/src/main/java/io/dddbyexamples/eventsource/eventstore
+///Users/songyancheng/Workspace/DDD/eventsourcing-java-example/eventsourcing/src/main/java/com/pragmatists/eventsourcing
+///Users/songyancheng/Workspace/DDD/java-cqrs-intro/cqrswithes/src/main/java/pl/altkom/asc/lab/cqrs/intro/cqrswithes/cqs
+///Users/songyancheng/Workspace/DDD/java-cqrs-intro/cqrswithes/src/main/java/pl/altkom/asc/lab/cqrs/intro/cqrswithes/db
