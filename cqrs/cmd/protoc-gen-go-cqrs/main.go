9db6c22da36101401c4200d5ae37ee76bb856f60
soyacen <soyacen@outlook.com>
2023-07-29 23:47:19 +0800

    add cmd dir

---
+++ cqrs/cmd/protoc-gen-go-cqrs/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/types/pluginpb"
+)
+
+const version = "v1.4.3"
+
+var pathToLower *bool
+var requireUnimplemented *bool
+
+func main() {
+	showVersion := flag.Bool("version", false, "print the version and exit")
+	flag.Parse()
+	if *showVersion {
+		fmt.Printf("protoc-gen-go-gors %v\n", version)
+		return
+	}
+
+	var flags flag.FlagSet
+	pathToLower = flags.Bool("path_to_lower", false, "make path to lower case")
+	requireUnimplemented = flags.Bool("require_unimplemented_servers", true, "set to false to match legacy behavior")
+	protogen.Options{ParamFunc: flags.Set}.Run(func(gen *protogen.Plugin) error {
+		gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+		for _, f := range gen.Files {
+			if !f.Generate {
+				continue
+			}
+			generateFile(gen, f)
+		}
+		return nil
+	})
+}
